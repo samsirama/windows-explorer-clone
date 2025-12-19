718d91a499da8d57d48de76eb514e64ec4cfca25
Samsi Rama <samsi@trillium-tech.com>
2025-12-19 15:25:33 +0700

    fix(vercel): remove edge runtime constraint

--- packages/server/api/index.ts
+++ packages/server/api/index.ts
@@ -1,8 +1,8 @@
 import { app } from "../src/index";
 
-export const config = {
-  runtime: "edge",
-};
+// export const config = {
+//   runtime: "edge",
+// };
 
 export default async function handler(request: Request) {
   return app.fetch(request);
